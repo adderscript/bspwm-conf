dd0de2dc536e28d3aff5f3a0198109e170d13e9a
Alasdair Gray <alasdairthgray@gmail.com>
2025-09-06 17:36:50 +0100

    initial commit

---
+++ themes/onedark/theme.bash
@@ -0,0 +1,82 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2025 Aditya Shakya <adi1090x@gmail.com>
+#
+# Onedark Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#000000'
+foreground='#ffffff'
+color0='#ffffff'
+color1='#ffffff'
+color2='#ffffff'
+color3='#ffffff'
+color4='#ffffff'
+color5='#ffffff'
+color6='#ffffff'
+color7='#ffffff'
+color8='#ffffff'
+color9='#ffffff'
+color10='#ffffff'
+color11='#ffffff'
+color12='#ffffff'
+color13='#ffffff'
+color14='#ffffff'
+color15='#ffffff'
+
+accent='#ffffff'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='JetBrains Mono:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Zafiro'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='arc.conf'
+geany_font='JetBrainsMono Nerd Font 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Arc-Dark'
+icon_theme='Zafiro'
+cursor_theme='Qogirr'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x48'
+dunst_origin='top-right'
+dunst_font='JetBrains Mono 10'
+dunst_border='2'
+dunst_separator='2'
+
+# Picom
+picom_backend='glx'
+picom_corner='0'
+picom_shadow_r='20'
+picom_shadow_o='0.60'
+picom_shadow_x='-20'
+picom_shadow_y='-20'
+picom_blur_method='none'
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="$accent"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='2'
+bspwm_gap='10'
+bspwm_sratio='0.50'
